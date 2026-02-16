d9ae977272c798d35d050e16ed5853b567cff127
Patrolin <patrik@balas.cz>
2026-02-16 13:06:30 +0100

    update linux build script

--- build.ps1
+++ build.ps1
@@ -3,4 +3,4 @@ rm ./qswagger.exe 2>$null
 echo "Building for Windows.."
 odin build qswagger -o:speed
 echo "Building for Linux.."
-wsl -- ~/Odin/odin build qswagger "-out:qswagger-linux-x64" "-o:speed"
+wsl -- odin build qswagger "-out:qswagger-linux-x64" "-o:speed"
